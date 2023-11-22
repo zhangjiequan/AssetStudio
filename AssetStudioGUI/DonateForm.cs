5ff504f77da15daaa8ddae200e1997fcf6dc31a1
zhangjiequan <zhangjiequan@aobi.com>
2023-11-22 11:38:55 +0800

    feat: add "donate with PayPal"

--- AssetStudioGUI/DonateForm.cs
+++ AssetStudioGUI/DonateForm.cs
@@ -11,19 +11,19 @@ public class DonateForm : Form
     private void InitializeComponent()
     {
         this.Icon = global::AssetStudioGUI.Properties.Resources._as;
-        this.Size = new Size(613, 420);
+        this.Size = new Size(840, 470);
         this.Text = "Donate to AssetStudio";
 
         Label label = new Label();
         label.Text = "AssetStudio is a free and open-source software.\nIf you like it and find it helpful, you can buy me a coffee!";
         label.AutoSize = false;
-        label.Size = new Size(613, 50);
+        label.Size = new Size(840, 50);
         label.Location = new Point(10, 10);
         label.Font = new Font(label.Font.FontFamily, 12, FontStyle.Regular);
         label.Padding = new Padding(0, 5, 0, 5);
 
         PictureBox pictureBox = new PictureBox();
-        pictureBox.Size = new Size(573, 306);
+        pictureBox.Size = new Size(800, 350);
         pictureBox.Location = new Point(10, 60);
         pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
         pictureBox.Image = global::AssetStudioGUI.Properties.Resources.donate;
